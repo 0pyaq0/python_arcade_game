9bc648d70f76e26cae622b46c89441e76f921f39
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-01-31 13:50:16 +0900

    ⚡ :: 키보드 이벤트 및 경계값 처리

--- arcadegame/main.py
+++ arcadegame/main.py
@@ -21,17 +21,55 @@ character_height = character_size[1] #캐릭터 세로 크기
 character_x_pos = (screen_width/2) - (character_width/2) #화면 가로 절반 크기에 해당하는 곳에 위치
 character_y_pos = screen_height - character_height #화면 세로 크기 가장 아래에 해당하는 곳에 위치
 
+#이동할 좌표
+to_x = 0
+to_y = 0
+
 #이벤트 루프
 running = True #게임 진행 체크
+
 while running :
     for event in pygame.event.get(): #어떤 이벤트가 발생하였는지 체크
         if event.type == pygame.QUIT: #창이 닫히는 이벤트 발생 체크
             running = False #게임 중지
 
+        if event.type == pygame.KEYDOWN: #키보드 키
+            if event.key == pygame.K_LEFT:
+                to_x -= 5
+            elif event.key == pygame.K_RIGHT:
+                to_x += 5
+            elif event.key == pygame.K_UP:
+                to_y -= 5
+            elif event.key == pygame.K_DOWN:
+                to_y += 5
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y = 0
+
+    
+    character_x_pos += to_x
+    character_y_pos += to_y
+
+    #가로 경계값 처리
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    #세로 경계값 처리
+    if character_y_pos < 0:
+        character_y_pos = 0
+    elif character_y_pos > screen_height - character_height:
+        character_y_pos = screen_height - character_height
+
     screen.blit(backgroud, (0, 0)) #배경 그리기
     screen.blit(character, (character_x_pos, character_y_pos))
 
     pygame.display.update() #게임 화면 다시 그리기
+
 
 #pygame 종료
 pygame.quit()
