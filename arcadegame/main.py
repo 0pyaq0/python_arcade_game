4f139f9b3e3e3196e6ac07c15bc805e5802459a8
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-01-31 13:57:33 +0900

    📝 :: FPS 설정

--- arcadegame/main.py
+++ arcadegame/main.py
@@ -10,6 +10,9 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 #화면 타이틀 설정
 pygame.display.set_caption("Arcade Game") #게임 이름
 
+#FPS
+clock = pygame.time.Clock()
+
 #배경 이미지 불러오기
 backgroud = pygame.image.load("img\\background.png")
 
@@ -25,23 +28,28 @@ character_y_pos = screen_height - character_height #화면 세로 크기 가장 
 to_x = 0
 to_y = 0
 
+#이동 속도
+character_speed = 0.6
+
 #이벤트 루프
 running = True #게임 진행 체크
 
 while running :
+    dt = clock.tick(10) #초당 프레임 수
+    
     for event in pygame.event.get(): #어떤 이벤트가 발생하였는지 체크
         if event.type == pygame.QUIT: #창이 닫히는 이벤트 발생 체크
             running = False #게임 중지
 
         if event.type == pygame.KEYDOWN: #키보드 키
             if event.key == pygame.K_LEFT:
-                to_x -= 5
+                to_x -= character_speed
             elif event.key == pygame.K_RIGHT:
-                to_x += 5
+                to_x += character_speed
             elif event.key == pygame.K_UP:
-                to_y -= 5
+                to_y -= character_speed
             elif event.key == pygame.K_DOWN:
-                to_y += 5
+                to_y += character_speed
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -50,8 +58,8 @@ while running :
                 to_y = 0
 
     
-    character_x_pos += to_x
-    character_y_pos += to_y
+    character_x_pos += to_x * dt
+    character_y_pos += to_y * dt
 
     #가로 경계값 처리
     if character_x_pos < 0:
